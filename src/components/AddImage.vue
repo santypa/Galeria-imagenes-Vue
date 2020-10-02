8349f5682853caa0f6bf64fe5213e1fcecfae797
g1alexander <alexlds26@gmail.com>
2020-10-02 14:45:56 -0500

    previsualizar imagen

--- src/components/AddImage.vue
+++ src/components/AddImage.vue
@@ -2,14 +2,22 @@
   <div id="addimage">
     <b-button v-b-modal.modal-1><font-awesome-icon :icon="myIcon"/></b-button>
 
-    <b-modal id="modal-1" title="BootstrapVue">
-      <input
-        type="file"
-        @change="previewFiles"
-        multiple
-        tabindex="-1"
-        accept="image/jpeg, image/png, image/jpg, image/gif"
-      />
+    <b-modal id="modal-1" title="Agrega una Imagen" hide-footer>
+      <img :src="image" id="image" alt="tu imagen" />
+      <form @submit="onSubmit">
+        <b-form-group>
+          <b-form-file
+            type="file"
+            ref="input_img"
+            v-model="form.file"
+            @change="previewFiles"
+            multiple
+            tabindex="-1"
+            accept="image/jpeg, image/png, image/jpg, image/gif"
+          ></b-form-file>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Subir</b-button>
+      </form>
     </b-modal>
   </div>
 </template>
@@ -22,18 +30,32 @@ export default {
   data() {
     return {
       myIcon: faPlus,
+      form: {
+        file: "",
+      },
+      image: "https://via.placeholder.com/150",
     };
   },
   components: {
     FontAwesomeIcon,
   },
   methods: {
-    previewFiles(event) {
-      // process your files, read as DataUrl or upload...
-      console.log(event.target.files);
+    previewFiles(e) {
+      const file = e.target.files[0];
+      this.image = URL.createObjectURL(file);
+    },
+    onSubmit(e) {
+      e.preventDefault();
+      console.log(this.form.file);
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style>
+#image {
+  margin-bottom: 1em;
+  width: 100%;
+  height: 300px;
+}
+</style>
